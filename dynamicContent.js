663688145c06bb042a9a7537f13ec5f9aa777100
Benjamin Wilburn <bwilburnstrength@gmail.com>
2016-05-24 15:34:26 -0500

    added dynamicContent.js

---
+++ dynamicContent.js
@@ -0,0 +1,30 @@
+var container = document.getElementById("listMusicView");
+var userInput = document.getElementById("addMusicView");
+
+var musicList = document.getElementById("listMusic").addEventListener("click", function () {
+    userInput.classList.add("displayNone");
+    container.classList.remove("displayNone");
+});
+
+var musicAdd = document.getElementById("addMusic").addEventListener("click", function () {
+	container.classList.add("displayNone");
+	userInput.classList.remove("displayNone");
+});
+
+
+
+
+
+
+// THIS FUNCTION DOESNT WORK BUT IT HELPED ME WRITE THE CALLBACK FUNCTIONS
+// function toggleDiv () {
+//   if (musicList.clicked == true) {
+//   	userInput.classList.add("displayNone");
+//   	container.classList.remove("displayNone");
+//   } else if (musicAdd.clicked == true){
+//     container.classList.add("displayNone");
+//     userInput.classList.remove("displayNone");
+//   } else {
+//   	userInput.classList.add("displayNone");
+//   }
+// }
